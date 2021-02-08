4797d4cfe8b3a23c2762fc231152d05bffee6525
Lengrube <70213478+Lengrube@users.noreply.github.com>
2021-02-08 12:38:40 -0300

    Mejoras al código
    Ahora cuando las carpetas necesarias se encuentran ausentes, el programa las crea por el usuario.

--- ordenar_carpeta.py
+++ ordenar_carpeta.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 # Aquí colocas tu ruta de descargas
-route_downloads = 'C:\\Users\\Elias\\Downloads\\'
+route_downloads = '/home/elias-lengrube/Descargas/'
 
 # Si quieres puedes agregar mas extensiones, como .bat, .ttf, .py, etx
 text_extensions = ('.txt', '.doc', '.docx', 'pptx', '.odf', '.docm', '.pdf')
@@ -12,7 +12,83 @@ photo_extensions = ('.jpg', '.png', 'jpeg', '.gif', '.tiff', '.psd', '.bmp', '.i
 compressed_extensions = ('.zip', '.rar', '.rar5', '.7z', '.ace', '.gz')
 executable_extensions = ('.exe', '.msi')
 
-# Cree las carpetas antes de ejecutar, si no, puede usar el módulo mkdir(), de os
+def get_option():
+    print('¿Deseas que el programa cree las carpetas faltantes?')
+    option = input('S/N >>').strip().lower()
+
+    attemps = 0
+
+    while option != 's' and option != 'n':
+
+        if attemps == 4:
+            print('')
+            print('Máximos intentos realizados.')
+            exit()
+
+        print('' )
+        print('Opción no válida, inserte una "S" o una "N" respectivamente')
+        option = input('>> ').strip().lower()
+
+        attemps += 1
+
+    return option
+
+def check_folders():
+    necessary = (
+            'Texto','Videos', 'Audios',
+            'Imagenes', 'Comprimidos', 'Ejecutables',
+            'Otros'
+            )
+
+    all_files = os.listdir(route_downloads)
+    missing_folders = []
+
+    for n in necessary:
+
+        len_files = len(all_files)
+        progress = 0
+        
+        for f in all_files:
+            if f != n:
+                progress += 1
+
+            else:
+                pass
+
+        if progress == len_files:
+            missing_folders.append(n)
+
+    if len(missing_folders) == 0:
+        pass
+
+    else:
+        print('Oh a ocurrido un error, en su directorio {} - faltan las siguientes carpetas: '.format(route_downloads))
+
+        for c in missing_folders:
+            print('* {}'.format(c))
+
+        print('')
+
+        option = get_option()
+        
+        if option == 'n':
+            print('')
+            exit()
+
+        else:
+            try:
+                for mf in missing_folders:
+                    # OJO AQUÍ CON LA SITAXIS DE LA RUTA
+                    os.mkdir(route_downloads + mf)
+                    
+                print('')
+                print('Carpetas creadas satisfactoriamente.')
+
+            except:
+                print('')
+                print('Oh, ah ocurrido un error inesperado.')
+
+
 def order(file, extension):
 
     for etx in text_extensions:
@@ -56,6 +132,8 @@ def order(file, extension):
 def main():
 
     print('Iniciando\n')
+
+    check_folders()
 
     for file in os.listdir(route_downloads):
 
